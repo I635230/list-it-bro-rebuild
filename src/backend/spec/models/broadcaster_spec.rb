40d400bacfb7b8c7ac9de9ba4b290105d4a4626a
I635230 <yugo.susuki@gmail.com>
2025-06-17 23:14:31 +0900

    :adhesive_bandage: エラーの原因を明確にするため、FactoryBotを使用するように変更

--- src/backend/spec/models/broadcaster_spec.rb
+++ src/backend/spec/models/broadcaster_spec.rb
@@ -2,13 +2,13 @@ require 'rails_helper'
 
 RSpec.describe Broadcaster, type: :model do
   it 'idなしでBroadcasterモデルが生成できない' do
-    broadcaster = Broadcaster.new(id: nil)
+    broadcaster = build(:broadcaster, id: nil)
     expect(broadcaster).not_to be_valid
   end
 
   it '同じidのBroadcasterモデルが登録できない' do
-    broadcaster1 = Broadcaster.create(id: 1)
-    broadcaster2 = Broadcaster.new(id: 1)
+    broadcaster1 = create(:broadcaster, id: 1)
+    broadcaster2 = build(:broadcaster, id: 1)
     expect(broadcaster2).not_to be_valid
   end
 end
