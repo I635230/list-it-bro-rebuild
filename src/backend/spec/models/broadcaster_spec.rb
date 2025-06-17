cabe840328b7b0f0c14865042e02cdb04f19217a
I635230 <yugo.susuki@gmail.com>
2025-06-17 22:59:51 +0900

    :adhesive_bandage: Broadcasterモデルのバリデーションのテストを修正

--- src/backend/spec/models/broadcaster_spec.rb
+++ src/backend/spec/models/broadcaster_spec.rb
@@ -2,13 +2,13 @@ require 'rails_helper'
 
 RSpec.describe Broadcaster, type: :model do
   it 'idなしでBroadcasterモデルが生成できない' do
-    user = User.new(id: nil)
-    expect(user).not_to be_valid
+    broadcaster = Broadcaster.new(id: nil)
+    expect(broadcaster).not_to be_valid
   end
 
   it '同じidのBroadcasterモデルが登録できない' do
-    user1 = User.create(id: nil)
-    user2 = User.new(id: nil)
-    expect(user2).not_to be_valid
+    broadcaster1 = Broadcaster.create(id: 1)
+    broadcaster2 = Broadcaster.new(id: 1)
+    expect(broadcaster2).not_to be_valid
   end
 end
