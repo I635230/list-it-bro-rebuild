f7ad7a4b6865127a21c9c1c0729b80e4b5609092
I635230 <yugo.susuki@gmail.com>
2025-06-22 00:11:25 +0900

    :sparkles: Twitch APIへリクエストするときのヘッダーを作成

--- src/backend/app/controllers/application_controller.rb
+++ src/backend/app/controllers/application_controller.rb
@@ -1,2 +1,19 @@
 class ApplicationController < ActionController::API
+  def twitch_api_header(type, current_user = nil)
+    if type == "app-access-token"
+      {
+        "Authorization" => ENV["APP_ACCESS_TOKEN"],
+        "Client-id" => ENV["CLIENT_ID"]
+      }
+    elsif type == "user-access-token"
+      {
+        "Authorization" => current_user.user_access_token,
+        "Client-id" => ENV["CLIENT_ID"]
+      }
+    elsif type == "www-form"
+      {
+        'Content-Type': "application/x-www-form-urlencoded"
+      }
+    end
+  end
 end
